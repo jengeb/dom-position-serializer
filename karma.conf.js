300d06b7bca521123999f849e780cecafa6551f8
Jenny Gebske <jennifer@gebske.de>
2015-12-14 22:36:03 +0100

    add tests

--- karma.conf.js
+++ karma.conf.js
@@ -10,11 +10,12 @@ module.exports = function(config) {
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['mocha'],
+    frameworks: ['mocha', 'chai'],
 
 
     // list of files / patterns to load in the browser
     files: [
+      'node_modules/jquery/dist/jquery.js',
       'index.js',
       'test/**/*Spec.js'
     ],
--- test/serializer.Spec.js
+++ test/serializer.Spec.js
@@ -1,5 +1,9 @@
 describe('serializePosition()', function() {
   it('should serialize a position', function() {
       // testcode TODO
+      $('body').append('<div id="root"><h1>Title</h1><div></div><div id="target"></div></div>');
+      var position = serializePosition($('#target')[0], 5, $('#root')[0], 'myClass');
+      expect(position.path[0]).to.equal(2);
+      expect(position.offset).to.equal(5);
   });
 });
