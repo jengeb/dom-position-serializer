c2ac06649d6a5741b9603b55df454416c1409ee2
Jenny Gebske <jennifer@gebske.de>
2015-12-18 13:05:17 +0100

    change test order

--- test/serializer.Spec.js
+++ test/serializer.Spec.js
@@ -3,6 +3,7 @@ beforeEach(function() {
   $('body').append('<div id="testContainer"></div>');
 });
 
+
 // serializePosition()
 describe('serializePosition()', function() {
   it('should serialize a position', function() {
@@ -25,63 +26,13 @@ describe('serializePosition()', function() {
         '</div>' +
       '</div>'
     );
+
     var position = serializePosition($('#target')[0], 5, $('#root')[0], 'myClass');
     expect(position.path).to.deep.equal([2]);
     expect(position.offset).to.equal(5);
   });
 });
 
-// deserializePosition()
-describe('deserializePosition()', function() {
-  it('should deserialize a position', function() {
-    $('#testContainer').append(
-      '<div id="root">' +
-        '<h1>' +
-          'Title' +
-        '</h1>' +
-        '<h2>' +
-          'myTest' +
-        '</h2>' +
-        '<div>' +
-          '<h1 id="target">' +
-            'My Test' +
-          '</h1>' +
-        '</div>' +
-      '</div>'
-    );
-    var element = {path: [2, 0, 0], offset: 0};
-    var result = deserializePosition(element, $('#root')[0], 'myClass');
-    expect(result.node).to.equal($('#target')[0].firstChild);
-    expect(result.node.textContent).to.equal('My Test');
-  })
-});
-
-describe('deserializePosition()', function() {
-  it('should deserialize a position and skip myClass elements', function() {
-    $('#testContainer').append(
-      '<div id="root">' +
-        '<h1>' +
-          'Title' +
-        '</h1>' +
-        '<h2>' +
-          'myTest' +
-        '</h2>' +
-        '<div>' +
-          '<h1 id="target">' +
-            '<span class="myClass">' +
-              'My' +
-            '</span>' +
-            'Test' +
-          '</h1>' +
-        '</div>' +
-      '</div>'
-    );
-    var element = {path: [2, 0, 0], offset: 0};
-    var result = deserializePosition(element, $('#root')[0], 'myClass');
-    expect(result.node).to.equal($('#target')[0].firstChild.firstChild);
-    expect(result.node.textContent).to.equal('My');
-  })
-});
 
 describe('serializePosition()', function() {
   it('should serialize a position and skip myClass elements in a html tree', function() {
@@ -120,11 +71,69 @@ describe('serializePosition()', function() {
         '</div>' +
       '</div>'
     );
+
     var position = serializePosition($('#list')[0].firstChild.firstChild, 0, $('#root')[0], 'myClass');
     expect(position.path).to.deep.equal([2, 1, 1, 0]);
     expect(position.offset).to.equal(0);
   })
 });
+
+
+// deserializePosition()
+describe('deserializePosition()', function() {
+  it('should deserialize a position', function() {
+    $('#testContainer').append(
+      '<div id="root">' +
+        '<h1>' +
+          'Title' +
+        '</h1>' +
+        '<h2>' +
+          'myTest' +
+        '</h2>' +
+        '<div>' +
+          '<h1 id="target">' +
+            'My Test' +
+          '</h1>' +
+        '</div>' +
+      '</div>'
+    );
+
+    var element = {path: [2, 0, 0], offset: 0};
+    var result = deserializePosition(element, $('#root')[0], 'myClass');
+    expect(result.node).to.equal($('#target')[0].firstChild);
+    expect(result.node.textContent).to.equal('My Test');
+  })
+});
+
+
+describe('deserializePosition()', function() {
+  it('should deserialize a position and skip myClass elements', function() {
+    $('#testContainer').append(
+      '<div id="root">' +
+        '<h1>' +
+          'Title' +
+        '</h1>' +
+        '<h2>' +
+          'myTest' +
+        '</h2>' +
+        '<div>' +
+          '<h1 id="target">' +
+            '<span class="myClass">' +
+              'My' +
+            '</span>' +
+            'Test' +
+          '</h1>' +
+        '</div>' +
+      '</div>'
+    );
+
+    var element = {path: [2, 0, 0], offset: 0};
+    var result = deserializePosition(element, $('#root')[0], 'myClass');
+    expect(result.node).to.equal($('#target')[0].firstChild.firstChild);
+    expect(result.node.textContent).to.equal('My');
+  })
+});
+
 
 describe('deserializePosition()', function() {
   it('should deserialize a position and skip myClass elements in a html tree', function() {
@@ -163,6 +172,7 @@ describe('deserializePosition()', function() {
         '</div>' +
       '</div>'
     );
+
     var element = {path: [2, 1, 1, 0], offset: 0};
     var result = deserializePosition(element, $('#root')[0], 'myClass');
     expect(result.node).to.equal($('#list')[0].firstChild.firstChild);
@@ -170,105 +180,10 @@ describe('deserializePosition()', function() {
   })
 });
 
-describe('deserializePosition()', function() {
-  it('should deserialize a position: case 1: class spans whole textnode', function() {
-    $('#testContainer').append(
-      '<div id="root">' +
-        '<div></div>' +
-        '<div>' +
-          '<h1 id="target">' +
-            '<span class="myClass">' +
-              'My example' +
-            '</span>' +
-          '</h1>' +
-        '</div>' +
-      '</div>'
-    );
-    var element = {path: [1, 0, 0], offset: 0};
-    var result = deserializePosition(element, $('#root')[0], 'myClass');
-    expect(result.node).to.equal($('#target')[0].firstChild.firstChild);
-    expect(result.node.textContent).to.equal('My example');
-  })
-});
-
-describe('deserializePosition()', function() {
-  it('should deserialize a position: case 2: span as firstChild', function() {
-    $('#testContainer').append(
-      '<div id="root">' +
-        '<div></div>' +
-        '<div>' +
-          '<h1 id="target">' +
-            '<span class="myClass">' +
-              'My' +
-            '</span>' +
-            ' example' +
-          '</h1>' +
-        '</div>' +
-      '</div>'
-    );
-    var element = {path: [1, 0, 0], offset: 0};
-    var result = deserializePosition(element, $('#root')[0], 'myClass');
-    expect(result.node).to.equal($('#target')[0].firstChild.firstChild);
-    expect(result.node.textContent).to.equal('My');
-
-    var element = {path: [1, 0, 0], offset: 4};
-    var result = deserializePosition(element, $('#root')[0], 'myClass');
-    expect(result.node).to.equal($('#target')[0].firstChild.nextSibling);
-    expect(result.node.textContent).to.equal(' example');
-
-  })
-});
-
-describe('deserializePosition()', function() {
-  it('should deserialize a position: case 3: span as lastChild', function() {
-    $('#testContainer').append(
-      '<div id="root">' +
-        '<div>' +
-        '</div>' +
-        '<div>' +
-          '<h1 id="target">' +
-            'My ' +
-            '<span class="myClass">' +
-              'example' +
-            '</span>' +
-          '</h1>' +
-        '</div>' +
-      '</div>'
-    );
-    var element = {path: [1, 0, 0], offset: 4};
-    var result = deserializePosition(element, $('#root')[0], 'myClass');
-    expect(result.node).to.equal($('#target')[0].firstChild.nextSibling.firstChild);
-    expect(result.node.textContent).to.equal('example');
-  })
-});
-
-describe('deserializePosition()', function() {
-  it('should deserialize a position: case 4: span in the middle', function() {
-    $('#testContainer').append(
-      '<div id="root">' +
-        '<div></div>' +
-        '<div>' +
-          '<h1 id="target">' +
-            'My ' +
-            '<span class="myClass">' +
-              'exa' +
-            '</span>' +
-            'mple' +
-          '</h1>' +
-        '</div>' +
-      '</div>'
-    );
-    var element = {path: [1, 0, 0], offset: 4};
-    var result = deserializePosition(element, $('#root')[0], 'myClass');
-    expect(result.node).to.equal($('#target')[0].firstChild.nextSibling.firstChild);
-    expect(result.node.textContent).to.equal('exa');
-  })
-});
-
 
 // serializePosition() + deserializePosition()
 describe('serializePosition() and deserializePosition()', function() {
-  it('should serialize and deserialize a position: without myClass-elements', function() {
+  it('should de-/serialize a position: without myClass-elements', function() {
     $('#testContainer').append(
       '<div id="root">' +
         '<h1>' +
@@ -288,6 +203,7 @@ describe('serializePosition() and deserializePosition()', function() {
         '</div>' +
       '</div>'
     );
+
     var newPosition = serializePosition($('#test')[0].firstChild.nextSibling.firstChild, 0, $('#root')[0], 'myClass');
     expect(newPosition.path).to.deep.equal([2, 1, 0, 1, 0]);
     expect(newPosition.offset).to.equal(0);
@@ -299,7 +215,33 @@ describe('serializePosition() and deserializePosition()', function() {
 });
 
 describe('serializePosition() and deserializePosition()', function() {
-  it('should deserialize a position: case 2: span as firstChild', function() {
+  it('should de-/serialize a position: case 1: class spans whole textnode', function() {
+    $('#testContainer').append(
+      '<div id="root">' +
+        '<div></div>' +
+        '<div>' +
+          '<h1 id="target">' +
+            '<span class="myClass">' +
+              'My example' +
+            '</span>' +
+          '</h1>' +
+        '</div>' +
+      '</div>'
+    );
+
+    var position = serializePosition($('#target')[0].firstChild.firstChild, 0, $('#root')[0], 'myClass');
+    expect(position.path).to.deep.equal([1, 0, 0]);
+    expect(position.offset).to.equal(0);
+
+    var result = deserializePosition(position, $('#root')[0], 'myClass');
+    expect(result.node).to.equal($('#target')[0].firstChild.firstChild);
+    expect(result.node.textContent).to.equal('My example');
+  })
+});
+
+
+describe('serializePosition() and deserializePosition()', function() {
+  it('should de-/serialize a position: case 2: span as firstChild', function() {
     $('#testContainer').append(
       '<div id="root">' +
         '<div></div>' +
@@ -332,8 +274,61 @@ describe('serializePosition() and deserializePosition()', function() {
   })
 });
 
+describe('deserializePosition()', function() {
+  it('should de-/serialize a position: case 3: span as lastChild', function() {
+    $('#testContainer').append(
+      '<div id="root">' +
+        '<div></div>' +
+        '<div>' +
+          '<h1 id="target">' +
+            'My ' +
+            '<span class="myClass">' +
+              'example' +
+            '</span>' +
+          '</h1>' +
+        '</div>' +
+      '</div>'
+    );
+
+    var position = serializePosition($('#target')[0].firstChild.nextSibling.firstChild, 0, $('#root')[0], 'myClass');
+    expect(position.path).to.deep.equal([1, 0, 0]);
+    expect(position.offset).to.equal(3);
+
+    var result = deserializePosition(position, $('#root')[0], 'myClass');
+    expect(result.node).to.equal($('#target')[0].firstChild.nextSibling.firstChild);
+    expect(result.node.textContent).to.equal('example');
+  })
+});
+
+describe('deserializePosition()', function() {
+  it('should de-/serialize a position: case 4: span in the middle', function() {
+    $('#testContainer').append(
+      '<div id="root">' +
+        '<div></div>' +
+        '<div>' +
+          '<h1 id="target">' +
+            'My ' +
+            '<span class="myClass">' +
+              'exa' +
+            '</span>' +
+            'mple' +
+          '</h1>' +
+        '</div>' +
+      '</div>'
+    );
+
+    var position = serializePosition($('#target')[0].firstChild.nextSibling.firstChild, 0, $('#root')[0], 'myClass');
+    expect(position.path).to.deep.equal([1, 0, 0]);
+    expect(position.offset).to.equal(3);
+
+    var result = deserializePosition(position, $('#root')[0], 'myClass');
+    expect(result.node).to.equal($('#target')[0].firstChild.nextSibling.firstChild);
+    expect(result.node.textContent).to.equal('exa');
+  })
+});
+
 describe('serializePosition() and deserializePosition()', function() {
-  it('should serialize and deserialize a position: run over other myClass-elements', function() {
+  it('should de-/serialize a position: run over other myClass-elements', function() {
     $('#testContainer').append(
       '<div id="root">' +
         '<div>' +
@@ -351,6 +346,7 @@ describe('serializePosition() and deserializePosition()', function() {
         '</h1>' +
       '</div>'
     );
+
     var newPosition = serializePosition($('#target')[0].firstChild.nextSibling.firstChild, 0, $('#root')[0], 'myClass');
     expect(newPosition.path).to.deep.equal([1, 0]);
     expect(newPosition.offset).to.equal(3);
@@ -361,9 +357,8 @@ describe('serializePosition() and deserializePosition()', function() {
   })
 });
 
-
 describe('serializePosition() and deserializePosition()', function() {
-  it('should serialize and deserialize a position: run over other myClass-elements 2', function() {
+  it('should de-/serialize a position: run over other myClass-elements 2', function() {
     $('#testContainer').append(
       '<div id="root">' +
         '<h1></h1>' +
@@ -383,6 +378,7 @@ describe('serializePosition() and deserializePosition()', function() {
         '</div>' +
       '</div>'
     );
+
     var newPosition = serializePosition($('#target')[0].firstChild.nextSibling.firstChild.nextSibling, 0, $('#root')[0], 'myClass');
     expect(newPosition.path).to.deep.equal([2, 0, 1, 0]);
     expect(newPosition.offset).to.equal(8);
